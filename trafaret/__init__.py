83ea882986d389268a0511a74116c457c1534887
litwisha <litwisha@gmail.com>
2017-03-12 21:18:57 +0200

    additional check to `_trafaret` method: walk deep inside to the root of trafaret just in case it can be a partial of Trafaret subclasses

--- trafaret/__init__.py
+++ trafaret/__init__.py
@@ -32,6 +32,16 @@ def _dd(value):
         return repr(value)
     return r"{%s}" % ', '.join("%r: %s" % (x[0], _dd(x[1])) for x in sorted(value.items(), key=lambda x: x[0]))
 
+def _issubclasspartial(fn, cls):
+    while True:
+        parent = fn
+        fn = getattr(parent, 'func', None)
+
+        if fn is None:
+            break
+
+    return issubclass(parent, cls)
+
 """
 Trafaret is tiny library for data validation
 It provides several primitives to validate complex data structures
@@ -169,7 +179,7 @@ class Trafaret(object):
         """
         if isinstance(trafaret, Trafaret) or inspect.isroutine(trafaret):
             return trafaret
-        elif issubclass(trafaret, Trafaret):
+        elif _issubclasspartial(trafaret, Trafaret):
             return trafaret()
         elif isinstance(trafaret, type):
             return Type(trafaret)
